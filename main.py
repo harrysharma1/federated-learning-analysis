f5c3687b53ea53b9b48353e9af49ca5a3ba9224f
Harry Sharma <harrysharma1066@gmail.com>
2024-11-11 12:12:03 +0000

    feat: add dlg implementation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import matplotlib.pyplot as plt
 from pprint import pprint
 
@@ -8,7 +9,7 @@ import torch.nn.functional as F
 from torch.autograd import grad
 from torchvision import transforms
 
-
+# Taken directly from (just testing): https://github.com/mit-han-lab/dlg/blob/master/models/vision.py
 class LetNet(nn.Module):
     def __init__(self):
         super(LetNet, self).__init__()
@@ -53,22 +54,25 @@ def label_to_onehot(target, num_classes=100):
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
 print("Device running on %s"%device)
 
-tp = transforms.ToTensor()
+
+tp = transforms.Compose([
+    transforms.Resize((32,32)),
+    transforms.ToTensor()
+])
 tt = transforms.ToPILImage()
 
 gt_data = Image.open('image/harry_ground_truth.JPG')
-
 gt_data = tp(gt_data).to(device)
-
 gt_data = gt_data.view(1, *gt_data.size())
+
 gt_label = torch.Tensor([0]).long().to(device)
 gt_label = gt_label.view(1, )
 gt_oneshot_label = label_to_onehot(gt_label)
 
 
 plt.imshow(tt(gt_data[0].cpu()))
-# plt.plot("Ground Truth")
-# plt.show()
+plt.plot("Ground Truth")
+plt.show()
 
 n_net = LetNet().to(device)
 
@@ -86,7 +90,46 @@ dummy_data = torch.randn(gt_data.size()).to(device).requires_grad_(True)
 dummy_label = torch.randn(gt_oneshot_label.size()).to(device).requires_grad_(True)
 
 plt.imshow(tt(dummy_data[0].cpu()))
-plt.plot("Check")
+plt.plot("Random Init")
 plt.show()
 
 optimizer = torch.optim.LBFGS([dummy_data, dummy_label])
+
+history = []
+
+for i in range(1000):
+    def closure():
+        optimizer.zero_grad()
+        
+        dummy_pred = n_net(dummy_data)
+        dummy_onehot_label = F.softmax(dummy_label, dim=-1)
+        dummy_loss = criterion(dummy_pred, dummy_onehot_label) 
+        dummy_dy_dx = torch.autograd.grad(dummy_loss, n_net.parameters(), create_graph=True)
+        
+        grad_diff = 0
+        for gx, gy in zip(dummy_dy_dx, original_dy_dx): 
+            grad_diff += ((gx - gy) ** 2).sum()
+        grad_diff.backward()
+        
+        return grad_diff
+    
+    optimizer.step(closure)
+    if i % 10 == 0: 
+        current_loss = closure()
+        print(i, "%.4f" % current_loss.item())
+        history.append(tt(dummy_data[0].cpu()))
+print(len(history))
+
+num_images = len(history)
+num_cols = 10
+num_rows = math.ceil(num_images / num_cols)  # Calculate the required number of rows
+
+plt.figure(figsize=(12, num_rows * 2.5))
+
+for i in range(100):
+    plt.subplot(num_rows, num_cols, i + 1)
+    plt.imshow(history[i])
+    plt.title("iter=%d" % (i * 10))
+    plt.axis('off')
+
+plt.show()
