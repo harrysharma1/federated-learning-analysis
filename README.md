4cf0f47ac0d38e6ebca48c6c1bc60ecf479e059a
Harry Sharma <harrysharma1066@gmail.com>
2024-11-11 14:55:58 +0000

    update: to replicate aspect of DLG that will be used in further analysis

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # federated-learning-analysis
 
 Testing for federated analysis from [DLG](https://github.com/mit-han-lab/dlg/tree/master).
+
+## Output
+
+![Figure_1](readme_images/Figure_1.png)
+![Figure_2](readme_images/Figure_2.png)
+![Figure_3](readme_images/Figure_3.png)
--- main.py
+++ main.py
@@ -9,10 +9,9 @@ import torch.nn.functional as F
 from torch.autograd import grad
 from torchvision import transforms
 
-# Taken directly from (just testing): https://github.com/mit-han-lab/dlg/blob/master/models/vision.py
-class LetNet(nn.Module):
+class LeNet(nn.Module):
     def __init__(self):
-        super(LetNet, self).__init__()
+        super(LeNet, self).__init__()
         act = nn.Sigmoid
         self.body = nn.Sequential(
             nn.Conv2d(3, 12, kernel_size=5, padding=5//2, stride=2),
@@ -32,62 +31,60 @@ class LetNet(nn.Module):
         # print(out.size())
         out = self.fc(out)
         return out
-    
-# Taken directly from (just testing): https://github.com/mit-han-lab/dlg/blob/master/models/vision.py
+
+        
 def weights_init(m):
-    if hasattr(m, "weight"):
+    if hasattr(m, 'weight'):
         m.weight.data.uniform_(-0.5, 0.5)
-    if hasattr(m, "bias"):
+    if hasattr(m, 'bias'):
         m.bias.data.uniform_(-0.5, 0.5)
-# Taken directly from, (just testing): https://github.com/mit-han-lab/dlg/blob/master/utils.py
+
 def cross_entropy_for_onehot(pred, target):
     return torch.mean(torch.sum(- target * F.log_softmax(pred, dim=-1), 1))
 
-# Taken directly from (just testing): https://github.com/mit-han-lab/dlg/blob/master/utils.py
-def label_to_onehot(target, num_classes=100):
-    target = torch.unsqueeze(target, 1)
-    onehot_target = torch.zeros(target.size(0), num_classes, device=target.device)
-    onehot_target.scatter_(1, target, 1)
-    return onehot_target
-
 
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
-print("Device running on %s"%device)
+print("Running on %s"%device)
 
 
 tp = transforms.Compose([
     transforms.Resize((32,32)),
     transforms.ToTensor()
 ])
+
 tt = transforms.ToPILImage()
 
-gt_data = Image.open('image/harry_ground_truth.JPG')
-gt_data = tp(gt_data).to(device)
-gt_data = gt_data.view(1, *gt_data.size())
+original_data = Image.open("image/harry_ground_truth.JPG")
+original_data = tp(original_data).to(device)
 
-gt_label = torch.Tensor([0]).long().to(device)
-gt_label = gt_label.view(1, )
-gt_oneshot_label = label_to_onehot(gt_label)
+original_data = original_data.view(1, *original_data.size())
+original_label = torch.Tensor([0]).long().to(device)
+original_label = original_label.view(1, )
+original_one_shot_label = F.one_hot(original_label, num_classes=100)
 
-
-plt.imshow(tt(gt_data[0].cpu()))
+plt.imshow(tt(original_data[0].cpu()))
 plt.plot("Ground Truth")
 plt.show()
 
-n_net = LetNet().to(device)
 
-torch.manual_seed(1066)
+n_net = LeNet().to(device)
+
+torch.manual_seed(1234)
 
 n_net.apply(weights_init)
+
 criterion = cross_entropy_for_onehot
 
-pred = n_net(gt_data)
-y = criterion(pred, gt_oneshot_label)
+# Original Gradient
+pred = n_net(original_data)
+y = criterion(pred, original_one_shot_label)
 dy_dx = torch.autograd.grad(y, n_net.parameters())
+
 original_dy_dx = list((_.detach().clone() for _ in dy_dx))
 
-dummy_data = torch.randn(gt_data.size()).to(device).requires_grad_(True)
-dummy_label = torch.randn(gt_oneshot_label.size()).to(device).requires_grad_(True)
+# Dummy Label and Data
+dummy_data = torch.randn(original_data.size()).to(device).requires_grad_(True)
+dummy_label = torch.randn(original_one_shot_label.size()).to(device).requires_grad_(True)
 
 plt.imshow(tt(dummy_data[0].cpu()))
 plt.plot("Random Init")
@@ -96,12 +93,11 @@ plt.show()
 optimizer = torch.optim.LBFGS([dummy_data, dummy_label])
 
 history = []
-
-for i in range(1000):
+for i in range(300):
     def closure():
         optimizer.zero_grad()
         
-        dummy_pred = n_net(dummy_data)
+        dummy_pred = n_net(dummy_data) 
         dummy_onehot_label = F.softmax(dummy_label, dim=-1)
         dummy_loss = criterion(dummy_pred, dummy_onehot_label) 
         dummy_dy_dx = torch.autograd.grad(dummy_loss, n_net.parameters(), create_graph=True)
@@ -118,18 +114,13 @@ for i in range(1000):
         current_loss = closure()
         print(i, "%.4f" % current_loss.item())
         history.append(tt(dummy_data[0].cpu()))
-print(len(history))
 
-num_images = len(history)
-num_cols = 10
-num_rows = math.ceil(num_images / num_cols)  # Calculate the required number of rows
-
-plt.figure(figsize=(12, num_rows * 2.5))
-
-for i in range(100):
-    plt.subplot(num_rows, num_cols, i + 1)
+plt.figure(figsize=(12, 8))
+for i in range(30):
+    plt.subplot(3, 10, i + 1)
     plt.imshow(history[i])
     plt.title("iter=%d" % (i * 10))
     plt.axis('off')
 
 plt.show()
+        
